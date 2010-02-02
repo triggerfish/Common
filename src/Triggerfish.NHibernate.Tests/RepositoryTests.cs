c740898d49f772a0e87474cb12029b4288860eb3
Peter Adams <peter.adams@triggerfishsoftware.co.uk>
2010-02-02 09:57:55 +0000

    Created

---
+++ src/Triggerfish.NHibernate.Tests/RepositoryTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NHibernate;
+using NHibernate.Validator.Exceptions;
+using Triggerfish.Validator;
+using NHibernate.Validator.Engine;
+
+namespace Triggerfish.NHibernate.Tests
+{
+	[TestClass]
+	public class RepositoryTests
+	{
+		Mock<ISession> m_session = new Mock<ISession>();
+		Mock<ITransaction> m_tx = new Mock<ITransaction>();
+
+		[TestMethod]
+		public void ShouldGetObject()
+		{
+			// arrange 
+			int id = 1;
+			string expected = "TestObject";
+			m_session.Setup(x => x.Get<string>(id)).Returns(expected);
+
+			Repository r = new Repository(m_session.Object);
+
+			// act
+			string s = r.Get<string>(id);			
+
+			// assert
+			Assert.AreEqual(expected, s);
+		}
+
+		[TestMethod]
+		public void ShouldReturnNullIfObjectDoesNotExist()
+		{
+			// arrange 
+			m_session.Setup(x => x.Get<string>(It.IsAny<object>())).Returns<string>(null);
+
+			Repository r = new Repository(m_session.Object);
+
+			// act
+			string s = r.Get<string>(1);
+
+			// assert
+			Assert.AreEqual(null, s);
+		}
+
+		[TestMethod]
+		public void ShouldDelete()
+		{
+			// arrange 
+			Repository r = new Repository(m_session.Object);
+			string s = "this";
+
+			// act
+			r.Delete<string>(s);
+
+			// assert
+			m_session.Verify(x => x.Delete((object)s));
+		}
+
+		[TestMethod]
+		public void ShouldSave()
+		{
+			// arrange 
+			Repository r = new Repository(m_session.Object);
+			string s = "this";
+
+			// act
+			r.Save<string>(s);
+
+			// assert
+			m_session.Verify(x => x.SaveOrUpdate((object)s));
+		}
+
+		[TestMethod]
+		public void ShouldSaveAll()
+		{
+			// arrange 
+			Repository r = new Repository(m_session.Object);
+			string[] s = new string[] {"this", "that"};
+
+			// act
+			r.Save<string>(s);
+
+			// assert
+			m_session.Verify(x => x.SaveOrUpdate(It.IsAny<object>()), Times.Exactly(2));
+		}
+
+		[TestInitialize]
+		public void SetupTest()
+		{
+			m_session.Setup(x => x.BeginTransaction()).Returns(m_tx.Object);
+		}
+	}
+}
---
+++ src/Triggerfish.NHibernate/Repository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NHibernate;
+using NHibernate.Linq;
+using Triggerfish.NHibernate;
+using NHibernate.Validator.Exceptions;
+using Triggerfish.FluentNHibernate;
+using Triggerfish.Linq;
+
+namespace Triggerfish.NHibernate
+{
+	/// <summary>
+	/// Represents an NHibernate repository
+	/// </summary>
+	public class Repository
+	{
+		/// <summary>
+		/// The NHibernate session
+		/// </summary>
+		public ISession Session { get; private set; }
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="session">A NHibernate session</param>
+		public Repository(ISession session)
+		{
+			Session = session;
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="session">A NHibernate session</param>
+		public Repository(IDbSession session)
+			: this(session.CreateSession())
+		{
+		}
+
+		/// <summary>
+		/// Get an object from the objects ID
+		/// </summary>
+		/// <typeparam name="T">Object type</typeparam>
+		/// <param name="id">Object ID</param>
+		/// <returns>The object</returns>
+		public virtual T Get<T>(int id)
+		{
+			return Session.Get<T>(id);
+		}
+
+		/// <summary>
+		/// Get a queryable list of objects
+		/// </summary>
+		/// <typeparam name="T">Object type</typeparam>
+		/// <returns>The objects</returns>
+		public virtual IOrderedQueryable<T> GetAll<T>()
+		{
+			return Session.Linq<T>();
+		}
+
+		/// <summary>
+		/// Deletes an object from the repository
+		/// </summary>
+		/// <typeparam name="T">Object type</typeparam>
+		/// <param name="target">the object</param>
+		public virtual void Delete<T>(T target)
+		{
+			Session.WithinTransaction(s => s.Delete(target));
+		}
+
+		/// <summary>
+		/// Saves or updates an object to the repository
+		/// </summary>
+		/// <typeparam name="T">Object type</typeparam>
+		/// <param name="target">The object to save or update</param>
+		public virtual void Save<T>(T target)
+		{
+			Session.WithinTransaction(s => s.SaveOrUpdate(target));
+
+			// what about if SaveOrUpdate throws?
+		}
+
+		/// <summary>
+		/// Saves or updates a list of objects to the repository
+		/// </summary>
+		/// <typeparam name="T">Object type</typeparam>
+		/// <param name="targets">The list of objects to save or update</param>
+		public virtual void Save<T>(IEnumerable<T> targets)
+		{
+			IEnumerable<object> objs = targets.Cast<object>();
+			Session.WithinTransaction(s => objs.ForEach(s.SaveOrUpdate));
+		}
+	}
+}
