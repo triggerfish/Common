c5ec8fb1ee561b016d10839a62dad2175221bfa4
Peter Adams <peter.adams@triggerfishsoftware.co.uk>
2010-03-15 11:03:46 +0000

    Added Hyperlink class

---
+++ src/Triggerfish.Web/Hyperlink.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Routing;
+
+namespace Triggerfish.Web
+{
+	/// <summary>
+	/// Represents the data required for a html hyperlink
+	/// </summary>
+	public class Hyperlink
+	{
+		/// <summary>
+		/// The type of http action the link performs
+		/// </summary>
+		public HttpAction ActionType { get; set; }
+
+		/// <summary>
+		/// The name of the link as seen by the user (anchor inner html)
+		/// </summary>
+		public string Text { get; set; }
+		
+		/// <summary>
+		/// The route values for the link
+		/// </summary>
+		public RouteValueDictionary Route { get; set; }
+		
+		/// <summary>
+		/// True if the link is selected (currently displayed), false otherwise
+		/// </summary>
+		public bool IsSelected { get; set; }
+	}
+}
---
+++ src/Triggerfish.Web/Mvc/HtmlExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Triggerfish.Web.Mvc
+{
+	/// <summary>
+	/// HtmlHElper extension methods
+	/// </summary>
+	public static class HtmlExtensions
+	{
+		/// <summary>
+		/// Creates a html anchor tag for the Hyperlink object
+		/// </summary>
+		/// <param name="html">Extension method on HtmlHelper</param>
+		/// <param name="link">The Hyperlink</param>
+		/// <returns>An html string</returns>
+		public static string RouteLink(this HtmlHelper html, Hyperlink link)
+		{
+			return html.RouteLink(link, null);
+		}
+
+		/// <summary>
+		/// Creates a html anchor tag for the Hyperlink object
+		/// </summary>
+		/// <param name="html">Extension method on HtmlHelper</param>
+		/// <param name="link">The Hyperlink</param>
+		/// <param name="cssSelectedClass">The CSS class name to apply if the link is selected</param>
+		/// <returns>An html string</returns>
+		public static string RouteLink(this HtmlHelper html, Hyperlink link, string cssSelectedClass)
+		{
+			Dictionary<string, object> attrs = new Dictionary<string, object>();
+			if (link.IsSelected && !String.IsNullOrEmpty(cssSelectedClass))
+			{
+				attrs.Add("class", cssSelectedClass);
+			}
+			return html.RouteLink(link.Text, link.Route, attrs);
+		}
+	}
+}
